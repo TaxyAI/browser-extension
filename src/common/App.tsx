3cbe84db87edaf59dc8f97eece517a549bb6c66b
Kyle Corbitt <kyle@corbt.com>
2023-03-27 11:31:40 -0700

    icon corners

--- src/common/App.tsx
+++ src/common/App.tsx
@@ -4,6 +4,9 @@ import { useAppState } from '../state/store';
 import ModelDropdown from './ModelDropdown';
 import SetKey from './SetKey';
 import TaskUI from './TaskUI';
+// import logo from '../assets/img/logo.svg';
+// import logo from '../assets/img/logo.svg';
+import logo from '../assets/img/icon-128.png';
 
 const App = ({ showIntro }: { showIntro?: boolean }) => {
   const openAIKey = useAppState((state) => state.settings.openAIKey);
@@ -12,6 +15,14 @@ const App = ({ showIntro }: { showIntro?: boolean }) => {
     <ChakraProvider>
       <Box p="8" fontSize="lg" w="full">
         <HStack mb={4} alignItems="center">
+          <img
+            src={logo}
+            width={32}
+            height={32}
+            className="App-logo"
+            alt="logo"
+          />
+
           <Heading as="h1" size="lg" flex={1}>
             Taxy AI
           </Heading>
