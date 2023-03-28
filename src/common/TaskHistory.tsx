7f2267e04f104fe89c188208487a947d420d812c
Kyle Corbitt <kyle@corbt.com>
2023-03-28 10:38:13 -0700

    fix some types

--- src/common/TaskHistory.tsx
+++ src/common/TaskHistory.tsx
@@ -22,7 +22,7 @@ type TaskHistoryItemProps = {
 
 const CollapsibleComponent = (props: {
   title: string;
-  subtitle: string | undefined;
+  subtitle?: string;
   text: string;
 }) => (
   <AccordionItem backgroundColor="white">
