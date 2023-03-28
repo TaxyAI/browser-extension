229a6ddea9af3fc908a3e5333b8a2eb193feb87e
David Corbitt <davidlcorbitt@gmail.com>
2023-03-28 10:03:56 -0700

    Add shorten_factor parameter to recording-to-gif.sh

--- demos/recording-to-gif.sh
+++ demos/recording-to-gif.sh
@@ -14,6 +14,14 @@ if [ ! -z "$2" ]; then
     scale=$2
 fi
 
+# By default, speed up the video by 4x
+shorten_factor=0.25
+
+# Check if user provided a speed-up parameter
+if [ ! -z "$3" ]; then
+    shorten_factor=$3
+fi
+
 # Get input file name without extension
 input_file="${1%.*}"
 
@@ -21,6 +29,6 @@ input_file="${1%.*}"
 output_file="$input_file.gif"
 
 # Use FFmpeg to make the GIF
-ffmpeg -i "$1" -vf "setpts=0.5*PTS,fps=15,scale=iw/${scale}:ih/${scale}:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 "$output_file"
+ffmpeg -i "$1" -vf "setpts=${shorten_factor}*PTS,fps=15,scale=iw/${scale}:ih/${scale}:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 "$output_file"
 
 echo "GIF conversion complete. Output file: $output_file"
