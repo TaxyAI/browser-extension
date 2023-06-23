df1e5b3def6efe33ed5ae4a6c9e5b26e64935656
pmdyy <mikedy7@gmail.com>
2023-06-23 14:23:46 -0700

    Add `gpt-3.5-turbo-16k` model

--- src/common/ModelDropdown.tsx
+++ src/common/ModelDropdown.tsx
@@ -20,8 +20,9 @@ const ModelDropdown = () => {
       value={selectedModel || ''}
       onChange={(e) => updateSettings({ selectedModel: e.target.value })}
     >
-      <option value="gpt-3.5-turbo">GPT-3.5 Turbo</option>
-      <option value="gpt-4">GPT-4</option>
+      <option value='gpt-3.5-turbo'>GPT-3.5 Turbo</option>
+      <option value='gpt-3.5-turbo-16k'>GPT-3.5 Turbo (16k)</option>
+      <option value='gpt-4'>GPT-4</option>
     </Select>
   );
 };
